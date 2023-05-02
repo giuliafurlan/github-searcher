ca91cd9b77cbf80eda5a6869b0f07497c310ee24
Giulia Furlan <giulia.furlan.dev@gmail.com>
2023-05-02 16:47:53 +0400

    moved font family declaration into app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+    weight: ['400', '500', '700'],
+    subsets: ['latin'],
+    variable: '--font-inter',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <main className={`${roboto.variable} font-sans`}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import SearchBar from '@/components/molecules/search-bar';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
     return (
         <main
-            className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+            className={`flex min-h-screen flex-col items-center justify-center content-center p-24 `}
         >
             <SearchBar />
         </main>
