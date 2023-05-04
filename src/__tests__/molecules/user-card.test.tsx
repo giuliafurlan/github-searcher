51f7e054ae852c5b9fe83b1faeca062625d0d30f
Giulia Furlan <giulia.furlan.dev@gmail.com>
2023-05-04 21:24:50 +0400

    fixed tests

--- src/__tests__/molecules/user-card.test.tsx
+++ src/__tests__/molecules/user-card.test.tsx
@@ -1,50 +1,38 @@
 import UserCard from '@/components/molecules/user-card';
 import { render, screen } from '@testing-library/react';
-import useSWR from 'swr';
 
 jest.mock('next/image', () => ({
     __esModule: true,
     default: jest.fn(),
 }));
 
-jest.mock('swr');
-
-const mockedSWR = useSWR as jest.MockedFunction<typeof useSWR>;
-
-const mockedSWRResult = {
-    data: null,
-    error: null,
-    mutate: jest.fn(),
-    isValidating: false,
-    isLoading: false,
-};
-
 describe('UserCard', () => {
     const user = {
         username: 'johndoe',
         imageSrc: 'https://example.com/avatar.jpg',
+        name: 'John Doe',
+        bio: 'Lorem ipsum dolor sit amet.',
+        location: 'New York, NY',
     };
 
     afterEach(() => {
         jest.clearAllMocks();
     });
 
-    it('displays the user name and bio', async () => {
-        const userData = {
-            name: 'John Doe',
-            bio: 'Lorem ipsum dolor sit amet.',
-            location: 'New York, NY',
-        };
-        mockedSWR.mockReturnValue({
-            ...mockedSWRResult,
-            data: userData,
-        });
-
+    it('displays the user name ', async () => {
         render(<UserCard {...user} />);
         expect(await screen.findByText('John Doe')).toBeInTheDocument();
+    });
+
+    it('displays the user  description', async () => {
+        render(<UserCard {...user} />);
         expect(
             screen.getByText('Lorem ipsum dolor sit amet.')
         ).toBeInTheDocument();
+    });
+
+    it('displays the user location', async () => {
+        render(<UserCard {...user} />);
         expect(screen.getByText('New York, NY')).toBeInTheDocument();
     });
 });
